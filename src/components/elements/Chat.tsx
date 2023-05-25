10cd51e780cea00dd688a2d8f4b2aa2c078219e6
SAEMMM <saem030@naver.com>
2023-05-25 17:02:22 +0900

    [Design] Main 퍼블리싱

--- src/components/elements/Chat.tsx
+++ src/components/elements/Chat.tsx
@@ -1,67 +1,74 @@
 import React, { useEffect, useState } from 'react';
-import { stompClient, sendMessage as sendStompMessage ,} from '../../pages/poorTalk/StompClient';
+import {
+  stompClient,
+  sendMessage as sendStompMessage,
+} from '../../pages/poorTalk/StompClient';
 
 interface Message {
-    content: string;
-  }
-  
-  // eslint-disable-next-line react/function-component-definition
-  const ChatComponent: React.FC = () => {
-    const [messages, setMessages] = useState<Message[]>([]);
-    const [newMessage, setNewMessage] = useState('');
-    const [connected, setConnected] = useState(false);
-  
-    const handleMessage = (message: Message) => {
-      setMessages((prevMessages) => [...prevMessages, message]);
-    };
-  
-    const sendMessage = () => {
-        const message: Message = { content: newMessage }; // 메시지 객체를 적절하게 구성해야 합니다.
-        sendStompMessage('/app/send', JSON.stringify(message)); // 수정된 부분
-        setNewMessage('');
-      };
-  
-      useEffect(() => {
-        stompClient.activate(); // 수정된 부분
-    
-        const onConnect = () => {
-          setConnected(true);
-          stompClient.subscribe('/topic/messages', (message) => {
-            handleMessage(JSON.parse(message.body));
-          });
-        };
-    
-        const onDisconnect = () => {
-          setConnected(false);
-        };
-    
-        stompClient.onConnect = onConnect;
-        stompClient.onDisconnect = onDisconnect;
-    
-        return () => {
-          stompClient.deactivate();
-          setConnected(false);
-        };
-      }, []);
-    return (
-      <div>
-         {connected ? <p>Connected to WebSocket</p> : <p>Not connected to WebSocket</p>}
-        <ul>
-          {messages.map((message, index) => (
-            // eslint-disable-next-line react/no-array-index-key
-            <li key={index}>{message.content}</li>
-          ))}
-        </ul>
-        <input
-          type="text"
-          value={newMessage}
-          onChange={(e) => setNewMessage(e.target.value)}
-        />
+  content: string;
+}
 
-        {/*  eslint-disable-next-line react/button-has-type */}
-        <button onClick={sendMessage}>Send</button>
-      </div>
-    );
+// eslint-disable-next-line react/function-component-definition
+const ChatComponent: React.FC = () => {
+  const [messages, setMessages] = useState<Message[]>([]);
+  const [newMessage, setNewMessage] = useState('');
+  const [connected, setConnected] = useState(false);
+
+  const handleMessage = (message: Message) => {
+    setMessages((prevMessages) => [...prevMessages, message]);
   };
-  
-  export default ChatComponent;
+
+  const sendMessage = () => {
+    const message: Message = { content: newMessage }; // 메시지 객체를 적절하게 구성해야 합니다.
+    sendStompMessage('/app/send', JSON.stringify(message)); // 수정된 부분
+    setNewMessage('');
+  };
+
+  useEffect(() => {
+    stompClient.activate(); // 수정된 부분
+
+    const onConnect = () => {
+      setConnected(true);
+      stompClient.subscribe('/topic/messages', (message) => {
+        handleMessage(JSON.parse(message.body));
+      });
+    };
+
+    const onDisconnect = () => {
+      setConnected(false);
+    };
+
+    stompClient.onConnect = onConnect;
+    stompClient.onDisconnect = onDisconnect;
+
+    return () => {
+      stompClient.deactivate();
+      setConnected(false);
+    };
+  }, []);
+  return (
+    <div>
+      {connected ? (
+        <p>Connected to WebSocket</p>
+      ) : (
+        <p>Not connected to WebSocket</p>
+      )}
+      <ul>
+        {messages.map((message, index) => (
+          // eslint-disable-next-line react/no-array-index-key
+          <li key={index}>{message.content}</li>
+        ))}
+      </ul>
+      <input
+        type="text"
+        value={newMessage}
+        onChange={(e) => setNewMessage(e.target.value)}
+      />
+
+      {/*  eslint-disable-next-line react/button-has-type */}
+      <button onClick={sendMessage}>Send</button>
+    </div>
+  );
+};
+
+export default ChatComponent;
--- src/pages/main/Main.tsx
+++ src/pages/main/Main.tsx
@@ -1,7 +1,57 @@
-import React from 'react';
+import React, { useState } from 'react';
+import '../../styles/pages/_Main.scss';
+import { FaPlus } from 'react-icons/fa';
 
 function Main() {
-  return <div>Main</div>;
+  const [account, setAccount] = useState(false);
+  console.log(setAccount);
+  return (
+    <div className="background">
+      <div className="title">
+        <p>내 가계부</p>
+        <h1>{!account ? '가계부를 만들어주세요' : '60,000원'}</h1>
+      </div>
+
+      {!account ? null : (
+        <div className="accountList">
+          <p>
+            가계부 <span>3</span>
+          </p>
+
+          <div className="account">
+            <div>
+              <p className="accountName">가계부 이름</p>
+              <p className="accountMoney">20,000원</p>
+            </div>
+            <div className="goAccountBtn">자세히</div>
+          </div>
+
+          <div className="account">
+            <div>
+              <p className="accountName">가계부 이름</p>
+              <p className="accountMoney">20,000원</p>
+            </div>
+            <div className="goAccountBtn">자세히</div>
+          </div>
+
+          <div className="account">
+            <div>
+              <p className="accountName">가계부 이름</p>
+              <p className="accountMoney">20,000원</p>
+            </div>
+            <div className="goAccountBtn">자세히</div>
+          </div>
+        </div>
+      )}
+
+      <div className="addAccountBtn">
+        가계부 추가
+        <div className="addAccountPlusBtn">
+          <FaPlus />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Main;
