d914d0d04b0032be93e77c348ec470d0dc6d2186
jinyoung <wlsud801@gmail.com>
2023-05-25 16:36:56 +0900

    [Feat] 버튼 컴포넌트 작성

--- src/pages/main/Main.tsx
+++ src/pages/main/Main.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import { Button } from '../../components';
 
 function Main() {
-  return <div>Main</div>;
+  return (
+  <div>
+    <Button className="common">제출하기</Button>
+  </div>
+  );
 }
 
 export default Main;
