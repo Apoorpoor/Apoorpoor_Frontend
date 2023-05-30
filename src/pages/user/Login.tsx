1da099372844b1bff6db291362f5594dbe73cf36
PC\PC <tokee678@daum.net>
2023-05-30 10:14:12 +0900

    [Test] 로그인 테스트

--- src/pages/user/Login.tsx
+++ src/pages/user/Login.tsx
@@ -1,22 +1,21 @@
-/* eslint-disable no-console */
-/* eslint-disable no-alert */
-/* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/no-use-before-define */
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable react/react-in-jsx-scope */
-/* eslint-disable no-restricted-globals */
-/* eslint-disable react/function-component-definition */
 import '../../styles/pages/_Login.scss';
 import { useNavigate } from "react-router-dom";
 import { useEffect } from 'react';
 import axios from 'axios';
+import Cookies from 'js-cookie';
 import Logo from '../../static/image/apoorpoorLogo.svg';
 import KakaoLogin from '../../static/image/login/kakao_login_medium_wide.png';
 import KAKAO_AUTH_URL from '../../api/Oauth';
 
 function Login() {
 
+  const JWTEXPIRYTIME = 24 * 3600 * 1000; // 만료 시간 (24시간 밀리 초로 표현)
   const navigate = useNavigate();
-
   useEffect(() => {
     const getAccessToken = async (urlCode: string): Promise<void> => {
       try {
@@ -25,8 +24,11 @@ function Login() {
         );
         const { accessToken } = response.headers.Authorization.split(" ")[1];
         const refreshToken = response.headers['Authorization-Refresh'].split(" ")[1];
+
+        // API 요청하는 콜마다 헤더에 accessToken 담아 보내도록 설정
+        // axios.defaults.headers.common.Authorization = `Bearer ${accessToken}`;
         localStorage.setItem('ACC_Token', accessToken);
-        localStorage.setItem('REF_Token', refreshToken);
+        Cookies.set('REF_Token', refreshToken, { expires: 7 });
         alert("로그인 성공");
         navigate(`/nickname`);
       } catch (error) {
@@ -34,6 +36,24 @@ function Login() {
         console.log(error);
       }
     };
+
+    const onSilentRefresh = () => {
+      axios.post('/silent-refresh')
+        .then(onLoginSuccess)
+        .catch(error => {
+          // ... 로그인 실패 처리
+        });
+    }
+
+    const onLoginSuccess = (response: { data: { accessToken: any; }; }) => {
+      const { accessToken } = response.data;
+
+      // accessToken 설정
+      axios.defaults.headers.common.Authorization = `Bearer ${accessToken}`;
+
+      // accessToken 만료하기 1분 전에 로그인 연장
+      setTimeout(onSilentRefresh, JWTEXPIRYTIME - 60000);
+    }
 
     const url = new URL(window.location.href);
     const urlCode = url.searchParams.get('code');
