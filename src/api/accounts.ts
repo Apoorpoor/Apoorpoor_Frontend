273065c7013359b823479fd65b3b79a1652b78ab
SAEMMM <saem030@naver.com>
2023-06-06 00:30:10 +0900

    [Feat] account 추가 get, post

--- src/api/accounts.ts
+++ src/api/accounts.ts
@@ -1,3 +1,27 @@
-// import axios from 'axios';
+import instance from './instance';
 
-export {};
+// 가계부 조회
+const getAccount = async () => {
+  try {
+    const response = await instance.get('/accounts');
+    console.log(response);
+    return response;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
+
+// 가계부 추가
+export const addAccount = async (title: string) => {
+  try {
+    const response = await instance.post('/account', { title });
+    console.log('가계부 추가 성공:', response);
+    return response.data;
+  } catch (error) {
+    console.log('가계부 추가 실패:', error);
+    throw error;
+  }
+};
+
+export default { getAccount, addAccount };
