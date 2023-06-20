3d17a73943c51397b6db14881196160c29180616
SAEMMM <saem030@naver.com>
2023-06-21 02:47:10 +0900

    [Design] 메인, 가계부 숫자 슬롯

--- src/components/elements/NumberAnimation.tsx
+++ src/components/elements/NumberAnimation.tsx
@@ -1,40 +1,89 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
+import '../../styles/components/_NumberAnimation.scss';
 
 interface NumberAnimationProps {
   targetNumber: number;
 }
 
 function NumberAnimation({ targetNumber }: NumberAnimationProps): JSX.Element {
-  // 숫자 증가 애니메이션
-  const [displayNumber, setDisplayNumber] = useState(0);
-  const duration = 3000;
+  const targetNumberOnly = Number(targetNumber.toString().replace('-', ''));
+  console.log('targetNumberOnly', targetNumberOnly);
+  const [animatedDigits, setAnimatedDigits] = useState<number[]>([]);
 
   useEffect(() => {
-    const startTime = performance.now();
-    let animationFrame: number;
+    const animationDuration = 1000; // 애니메이션 지속 시간 (1초)
+    const intervalDelay = 50; // 애니메이션 갱신 간격 (0.08초)
 
-    const animate = (timestamp: number) => {
-      const elapsedTime = timestamp - startTime;
-      let progress = elapsedTime / duration;
+    // 랜덤 배열 생성
+    const generateRandomDigits = (num: number): number[] => {
+      const digits = Array.from(String(num), Number);
+      console.log('digits:', digits);
+      const digitsLength = digits.length;
+      console.log('digitsLength:', digitsLength);
+      const randomDigits: number[] = [];
 
-      if (progress > 1) progress = 1;
+      for (let i = 0; i < digitsLength; i += 1) {
+        const randomDigit = Math.floor(Math.random() * 10);
+        randomDigits.push(randomDigit);
+        console.log('randomDigits:', randomDigits);
+      }
 
-      const nextNumber = Math.floor(targetNumber * progress);
-      setDisplayNumber(nextNumber);
+      return randomDigits;
+    };
 
-      if (progress < 1) {
-        animationFrame = requestAnimationFrame(animate);
+    // 숫자 변화 애니메이션
+    let currentFrame = 0;
+    let animationTimeout: NodeJS.Timeout | null = null;
+
+    const animateNumberAnimation = () => {
+      const randomDigits = generateRandomDigits(targetNumberOnly);
+      setAnimatedDigits(randomDigits);
+
+      if (currentFrame < animationDuration / intervalDelay) {
+        currentFrame += 1;
+        animationTimeout = setTimeout(animateNumberAnimation, intervalDelay);
       } else {
-        setDisplayNumber(targetNumber);
+        const finalDigits = Array.from(String(targetNumberOnly), Number);
+        setAnimatedDigits(finalDigits);
+        animationTimeout = null;
       }
     };
 
-    animationFrame = requestAnimationFrame(animate);
+    animateNumberAnimation();
 
-    return () => cancelAnimationFrame(animationFrame);
-  }, [targetNumber, duration]);
+    return () => {
+      if (animationTimeout) {
+        clearTimeout(animationTimeout);
+      }
+    };
+  }, [targetNumberOnly]);
 
-  return <>{displayNumber.toLocaleString()}</>;
+  const arrayDigits = Array.from(animatedDigits);
+  console.log('arrayDigits:', arrayDigits);
+
+  // 배열의 고유 key
+  let key = 0;
+
+  return (
+    <span id="numberAnimation">
+      {targetNumber < 0 ? '-' : ''}
+      {arrayDigits.map((item, index) => {
+        const uniqueKey = `${(key += 1)}`;
+        const comma =
+          index > 0 &&
+          (arrayDigits.length - index) % 3 === 0 &&
+          index !== arrayDigits.length - 1
+            ? ','
+            : '';
+        const formattedItem = `${comma}${item}`;
+        return (
+          <React.Fragment key={uniqueKey}>
+            <span className="number">{formattedItem}</span>
+          </React.Fragment>
+        );
+      })}
+    </span>
+  );
 }
 
 export default NumberAnimation;
