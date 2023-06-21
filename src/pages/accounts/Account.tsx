aed2ecc10e826b96a8c9d76b87862af17c580c41
SAEMMM <saem030@naver.com>
2023-06-21 16:52:03 +0900

    [Feat] 가계부 필터링 후, 스크롤 위치 복원

--- src/pages/accounts/Account.tsx
+++ src/pages/accounts/Account.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import '../../styles/pages/_Account.scss';
 import { AiFillCaretLeft, AiFillCaretRight } from 'react-icons/ai';
@@ -77,6 +77,9 @@ function Account(): JSX.Element {
   const navigateToPreviousPage = () => {
     navigate('/');
   };
+
+  // 스크롤 위치 저장
+  const categoryRef = useRef<HTMLDivElement>(null);
 
   // 내역 추가 버튼
   const goAddAccount = () => {
@@ -482,6 +485,13 @@ function Account(): JSX.Element {
     }
   );
 
+  // 데이터 호출할 때마다 스크롤 위치 복원
+  useEffect(() => {
+    if (currentString) {
+      categoryRef?.current?.scrollIntoView({ behavior: 'smooth' });
+    }
+  }, [currentString, getAccountType]);
+
   const [bottomObserveRef, bottomInView] = useInView({
     threshold: 0,
   });
@@ -693,7 +703,7 @@ function Account(): JSX.Element {
       <div className="line"> </div>
 
       <div className="_AccountBackground">
-        <div className="accountRangeBtn">
+        <div className="accountRangeBtn" ref={categoryRef}>
           {term.map((el, i) => (
             <div
               key={el.name}
