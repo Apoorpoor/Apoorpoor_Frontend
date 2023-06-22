62041494a81dc669314b6f02da4279342df202bc
PC\PC <tokee678@daum.net>
2023-06-22 22:16:09 +0900

    [Refactor] 아바타 레벨 10 까지 수정

--- src/pages/social/Reduction.tsx
+++ src/pages/social/Reduction.tsx
@@ -50,6 +50,10 @@ function Reduction() {
     4: 1,
     5: 5,
     6: 6,
+    7: 7,
+    8: 8,
+    9: 9,
+    10: 10
   };
 
   if (isLoading) {
@@ -81,7 +85,7 @@ function Reduction() {
           <img
             className="BasicBackGroundimage"
             src={BasicBackGround}
-            alt="배경"
+            alt=""
           />
           <div className="Top3Poor2">
             {topList && topList[1] && topList[1].level && (
@@ -89,76 +93,76 @@ function Reduction() {
                 className="Top2Poor"
                 src={`https://apoorapoors3.s3.ap-northeast-2.amazonaws.com/poor/poor_lv${poorCharacter[topList[1].level]
                   }.svg`}
-                alt="2등거지"
+                alt=""
               />
             )}
             {topList && topList[1] && topList[1].top_url && (
               <img
                 className="Top2PoorItem"
                 src={topList[1].top_url}
-                alt="2등거지 아이템"
+                alt=""
               />
             )}
             {topList && topList[1] && topList[1].acc_url && (
               <img
                 className="Top2PoorItem"
                 src={topList[1].acc_url}
-                alt="2등거지 아이템"
+                alt=""
               />
             )}
           </div>
-          <img className="SilverMedal" src={SilverMedal} alt="은메달" />
+          <img className="SilverMedal" src={SilverMedal} alt="" />
           <div className="Top3Poor3">
             {topList && topList[2] && topList[2].level && (
               <img
                 className="Top3Poor"
                 src={`https://apoorapoors3.s3.ap-northeast-2.amazonaws.com/poor/poor_lv${poorCharacter[topList[2].level]
                   }.svg`}
-                alt="3등거지"
+                alt=""
               />
             )}
             {topList && topList[2] && topList[2].top_url && (
               <img
                 className="Top3PoorItem"
                 src={topList[2].top_url}
-                alt="3등거지 아이템"
+                alt=""
               />
             )}
             {topList && topList[2] && topList[2].acc_url && (
               <img
                 className="Top3PoorItem"
                 src={topList[2].acc_url}
-                alt="3등거지 아이템"
+                alt=""
               />
             )}
           </div>
-          <img className="CopperMedal" src={CopperMedal} alt="동메달" />
-          <img className="Top1Crown" src={Crown} alt="왕관" />
+          <img className="CopperMedal" src={CopperMedal} alt="" />
+          <img className="Top1Crown" src={Crown} alt="" />
           <div className="Top3Poor1">
             {topList && topList[0] && topList[0].level && (
               <img
                 className="Top1Poor"
                 src={`https://apoorapoors3.s3.ap-northeast-2.amazonaws.com/poor/poor_lv${poorCharacter[topList[0].level]
                   }.svg`}
-                alt="1등거지"
+                alt=""
               />
             )}
             {topList && topList[0] && topList[0].top_url && (
               <img
                 className="Top1PoorItem"
                 src={topList[0].top_url}
-                alt="1등거지 아이템"
+                alt=""
               />
             )}
             {topList && topList[0] && topList[0].acc_url && (
               <img
                 className="Top1PoorItem"
                 src={topList[0].acc_url}
-                alt="1등거지 아이템"
+                alt=""
               />
             )}
           </div>
-          <img className="GoldMedal" src={GoldMedal} alt="금메달" />
+          <img className="GoldMedal" src={GoldMedal} alt="" />
         </div>
       </div>
       <div className="Top1Number">1</div>
@@ -196,17 +200,17 @@ function Reduction() {
                     className="Top10Image"
                     src={`https://apoorapoors3.s3.ap-northeast-2.amazonaws.com/poor/poor_lv${poorCharacter[item.level]
                       }.svg`}
-                    alt="푸어 이미지"
+                    alt=""
                   />
                   <img
                     className="Top10ImageItem"
                     src={item.top_url}
-                    alt="푸어 상의"
+                    alt=""
                   />
                   <img
                     className="Top10ImageItem"
                     src={item.acc_url}
-                    alt="푸어 악세서리"
+                    alt=""
                   />
                 </div>
                 {item.nickname}
