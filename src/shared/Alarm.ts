f756932cb195c2eb8c72e6a4d3254e32f08a24e7
jinyoung <wlsud801@gmail.com>
2023-07-17 16:31:12 +0900

    [Feat]persist Recoil 적용

--- src/shared/Alarm.ts
+++ src/shared/Alarm.ts
@@ -1,8 +1,15 @@
 import { atom, RecoilState } from 'recoil';
+import { recoilPersist } from 'recoil-persist';
 
-const alarmMessageState: RecoilState<string> = atom<string>({
+const { persistAtom } = recoilPersist();
+
+export const alarmMessageState: RecoilState<string> = atom<string>({
   key: 'alarmState',
   default: '알림 없음',
 });
 
-export default alarmMessageState;
+export const isConnected = atom({
+  key: 'isConnected',
+  default: false,
+  effects_UNSTABLE: [persistAtom]
+})
